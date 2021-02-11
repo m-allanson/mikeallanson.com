9f41250cb44116b9fb8e4700d28b5eac96e2dc54
m-allanson <michael.allanson@gmail.com>
2021-02-11 13:25:33 +0000

    Placeholder meta tags

--- src/page-wrapper.js
+++ src/page-wrapper.js
@@ -15,7 +15,19 @@ export default function PageWrapper(props) {
   return (
     <MDXProvider components={components}>
       <Helmet>
+        <html lang="en" />
         <link rel="stylesheet" href="/styles.css" />
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🪲</text></svg>"
+        ></link>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="description"
+          content="TODO!! Here is my lovely placeholder description"
+        />
+        <title>🪲🪲🪲</title>
       </Helmet>
       <header role="banner">
         <a href="/" aria-label="Mike Allanson">
