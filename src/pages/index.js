ca129d27587465df211e8185a15082c565b4e49b
m-allanson <michael.allanson@gmail.com>
2021-02-11 13:25:43 +0000

    Blah blah

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,18 @@ import { h } from "preact";
 export default ({ posts }) => (
   <div>
     <h1>Notes</h1>
-    <ul></ul>
-    {posts &&
-      posts.map((post, index) => (
-        <li key={post.slug}>
-          <a href={`/${post.slug}`}>{post.title}</a>
-        </li>
-      ))}
+    <ul>
+      {posts &&
+        posts.map((post, index) => (
+          <li key={post.slug}>
+            <a
+              style={{ padding: "2rem", display: "block" }}
+              href={`/${post.slug}`}
+            >
+              {post.title}
+            </a>
+          </li>
+        ))}
+    </ul>
   </div>
 );
