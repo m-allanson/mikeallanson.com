fb8cf469020d75f6d5029f6c09abc545adab5cf5
m-allanson <michael.allanson@gmail.com>
2021-08-17 09:06:37 +0100

    Re-jig anchor links on headings

--- src/modify-heading-anchors.js
+++ src/modify-heading-anchors.js
@@ -1,34 +1,44 @@
-"use strict";
-
-import has from "hast-util-has-property";
 import rank from "hast-util-heading-rank";
 import visit from "unist-util-visit";
 
-// Add 'aria-labelledBy' attribute
-// Add 'is' attribute
-// Replace child elements with a <span>#</span> element
 export default function label() {
+  /*
+    Toast's default headings look something like this:
+
+    <h2 class="" id="loading-modules-in-the-browser">
+      <a class="headingLinkAnchor" href="#loading-modules-in-the-browser">
+        <heading-link-icon class="heading-link-icon"></heading-link-icon>
+      </a>
+      Loading Modules in the Browser
+    </h2>
+
+    I want to put the heading text inside the anchor, and to remove the <heading-link-icon> element. Like this:
+
+    <h2 is="postHeading" id="loading-modules-in-the-browser">
+      <a href="#loading-modules-in-the-browser">
+        Loading Modules in the Browser
+      </a>
+    </h2>
+
+    No link is needed for h1's, as they are already the post title. They can have this structure:
+
+    <h1 is="postHeading" id="loading-modules-in-the-browser">
+        Loading Modules in the Browser
+    </h1>
+  */
+
   return (tree) =>
-    visit(tree, "element", (node, index, parent) => {
-      if (rank(parent) && has(parent, "id") && node.tagName === "a") {
-        delete node.properties.className;
-        node.properties = {
-          ...node.properties,
-          ariaLabelledBy: parent.properties.id,
-          is: "headingLinkAnchor",
-        };
-        node.children = [
-          {
-            type: "element",
-            tagName: "span",
-            children: [
-              {
-                type: "text",
-                value: "#",
-              },
-            ],
-          },
-        ];
+    visit(tree, "element", (node /*, index, parent*/) => {
+      if (rank(node)) {
+        const text = node.children.find((n) => n.type === "text");
+        const anchor = node.children.find((n) => n.tagName === "a");
+
+        node.properties.is = "postHeading";
+        anchor.children = [text];
+        delete anchor.properties.className;
+
+        // add links to h2 -> h6 only
+        node.children = rank(node) > 1 ? [anchor] : [text];
         return node;
       }
     });
