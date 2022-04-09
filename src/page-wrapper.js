d092e6b98afec48d3d6fc001ccb58247622ee28f
m-allanson <github@mikeallanson.com>
2022-04-09 21:30:32 +0100

    chore: Fix typo and bad link

--- src/page-wrapper.js
+++ src/page-wrapper.js
@@ -40,7 +40,6 @@ export default function PageWrapper(props) {
           rel="icon"
           href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🪲</text></svg>"
         />
-        <link rel="alternate icon" href="/favicon.ico" />
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta
